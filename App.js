ed4a2cd9d1989b16eeb3a6cca5ba2c9910d66cd5
geraldojr <geraldojrcg@gmail.com>
2018-11-20 10:46:54 -0300

    menu feito, falta colocar icone

--- App.js
+++ App.js
@@ -1,13 +1,84 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, TouchableOpacity } from 'react-native';
 import AppNavigator from './src/navigation/appNavigator';
 import './src/config/StatusBarConfig'
 import SideMenu from 'react-native-side-menu';
+import IOSIcon from "react-native-vector-icons/Ionicons";
+import Menu from './src/screens/Menu';
 
 export default class App extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.toggle = this.toggle.bind(this);
+
+    this.state = {
+      isOpen: false,
+      selectedItem: 'About',
+    };
+  }
+
+  toggle() {
+    this.setState({
+      isOpen: !this.state.isOpen,
+    });
+  }
+
+  updateMenuState(isOpen) {
+    this.setState({ isOpen });
+  }
+
+  onMenuItemSelected = item =>
+    this.setState({
+      isOpen: false,
+      selectedItem: item,
+    });
+
   render() {
+    const menu = <Menu onItemSelected={this.onMenuItemSelected} />;
     return (
-      <AppNavigator />
+      <SideMenu
+        menu={menu}
+        isOpen={this.state.isOpen}
+        onChange={isOpen => this.updateMenuState(isOpen)}
+      >
+        <TouchableOpacity
+          onPress={this.toggle}
+          style={styles.button}
+        >
+        <IOSIcon name="ios-menu" size={30} />
+        </TouchableOpacity>
+        <AppNavigator />
+      </SideMenu>
     );
   }
 }
+const styles = StyleSheet.create({
+  button: {
+    position: 'absolute',
+    top: 30,
+    padding: 10,
+  },
+  caption: {
+    fontSize: 20,
+    fontWeight: 'bold',
+    alignItems: 'center',
+  },
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#F5FCFF',
+  },
+  welcome: {
+    fontSize: 20,
+    textAlign: 'center',
+    margin: 10,
+  },
+  instructions: {
+    textAlign: 'center',
+    color: '#333333',
+    marginBottom: 5,
+  },
+});
+
